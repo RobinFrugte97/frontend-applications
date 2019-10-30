2f9c7e0573527f8c52c687ee76b065f3a9d3e1b3
Robin Frugte <robin.frugte@hotmail.com>
2019-10-30 14:05:22 +0100

    Fixed variable naming, removed some unused code

--- src/App.svelte
+++ src/App.svelte
@@ -4,15 +4,9 @@
   	import { onMount } from "svelte";
 	import NavLink from "./components/NavLink.svelte";
 	import Home from "./routes/Home.svelte";
-	import About from "./routes/About.svelte";
 	import Detail from "./routes/Detail.svelte";
 
-
-
-  // Used for SSR. A falsy value is ignored by the Router.
-
 	const queryUrl ="https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-08/sparql"
-	//Note that the query is wrapped in es6 template strings to allow for easy copy pasting
 	const query = `
 	PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 	PREFIX dc: <http://purl.org/dc/elements/1.1/>
@@ -20,11 +14,11 @@
 	PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 	PREFIX edm: <http://www.europeana.eu/schemas/edm/>
 	PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-	SELECT DISTINCT ?cho ?modelNaam ?placeName ?imageModel WHERE {
+	SELECT DISTINCT ?cho ?modelName ?placeName ?imageModel WHERE {
 	   <https://hdl.handle.net/20.500.11840/termmaster7745> skos:narrower* ?place .
 	   ?place skos:prefLabel ?placeName .
 	   VALUES ?type {"huismodel" "Huismodel"} .
-	   ?cho dc:title ?modelNaam ;
+	   ?cho dc:title ?modelName ;
 	        dc:type ?type ;
 	        dct:spatial ?place ;
 	        edm:isShownBy ?imageModel .
@@ -33,8 +27,6 @@
 
 	let data = [];
 	function runQuery(queryUrl, query){
-	  //Test if the endpoint is up and print result to page
-	  // (you can improve this script by making the next part of this function wait for a succesful result)
 	  fetch(queryUrl+"?query="+ encodeURIComponent(query) +"&format=json")
 	  .then(res => res.json())
 	  .then(json => {
@@ -54,7 +46,6 @@
     <NavLink to="/">Home</NavLink>
   </nav>
   <div>
-    <Route path="about" component="{About}" />
     <Route path="/" component="{Home}" data={data} />
 	<Route path="/details/*" component="{Detail}" />
   </div>
--- src/routes/Detail.svelte
+++ src/routes/Detail.svelte
@@ -1,47 +1,40 @@
 <script>
-  	import { onMount } from "svelte";
-	const queryUrl2 ="https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-08/sparql"
-	let modelCho = window.location.pathname.slice(-6);
-	let queryModel = ``;
-	let dataModel = [];
-	console.log(dataModel);
+
+	const queryUrl ="https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-08/sparql"
+	let detailURI = window.location.pathname.slice(-6);
+	let detailQuery = ``;
+	let detailData = [];
 	
-	function getData(modelCho) {
-		queryModel = `
+	function formulateQuery(detailURI) {
+		detailQuery = `
 		PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 		PREFIX dc: <http://purl.org/dc/elements/1.1/>
 		PREFIX dct: <http://purl.org/dc/terms/>
 		PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 		PREFIX edm: <http://www.europeana.eu/schemas/edm/>
 		PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-		SELECT ?cho ?modelNaam ?placeName ?imageModel WHERE {
-		   <https://hdl.handle.net/20.500.11840/` + modelCho + `> dc:title ?modelNaam ;
+		SELECT ?cho ?modelName ?placeName ?imageModel WHERE {
+		   <https://hdl.handle.net/20.500.11840/` + detailURI + `> dc:title ?modelName ;
 		        dc:type ?type ;
 		        dct:spatial ?place ;
 		        edm:isShownBy ?imageModel .
 		} LIMIT 1
 		`
-		console.log(queryModel);
 	}
-	async function runQueryModel(queryUrl2, queryModel){
-		console.log(queryUrl2);
-		console.log(queryModel);
-		
-		
-	  //Test if the endpoint is up and print result to page
-	  // (you can improve this script by making the next part of this function wait for a succesful result)
-	  await fetch(queryUrl2+"?query="+ encodeURIComponent(queryModel) +"&format=json")
-	  .then(res => res.json())
-	  .then(json => {
-		  dataModel = json.results.bindings
-		  console.log(dataModel[0]);
-	  })
+	async function runDetailQuery(queryUrl, detailQuery){
+
+		await fetch(queryUrl+"?query="+ encodeURIComponent(detailQuery) +"&format=json")
+		.then(res => res.json())
+		.then(json => {
+			detailData = json.results.bindings
+			console.log(detailData[0]);
+		})
 	}
-	getData(modelCho);
-	runQueryModel(queryUrl2, queryModel)
+	formulateQuery(detailURI);
+	runDetailQuery(queryUrl, detailQuery)
 			
 </script>
-{#if dataModel.length == 1}
-	<h1>{dataModel[0].modelNaam.value}</h1>
-	<img src={dataModel[0].imageModel.value} alt="">
+{#if detailData.length == 1}
+	<h1>{detailData[0].modelName.value}</h1>
+	<img src={detailData[0].imageModel.value} alt="">
 {/if}
