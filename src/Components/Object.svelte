37c7232c9b9e1769ca49dbbd8638b75d334e18bb
Robin Frugte <robin.frugte@hotmail.com>
2019-10-31 10:10:27 +0100

    Put homepage objects in its own component

---
+++ src/Components/Object.svelte
@@ -0,0 +1,58 @@
+<script>
+    export let link;
+    export let imageSource;
+    export let objectTitle;
+</script>
+<style>
+    li {
+		position: absolute;
+		width: 14rem;
+		display: flex;
+		flex-direction: column;
+		height: 13em;
+		background-color: #efefef;
+		border-radius: 0px 5% 5% 5%;
+		transition: .5s ease;
+	}
+	li:hover {
+		transform: scale(1.05);
+
+	}
+	li:nth-of-type(1){
+		top: 68%;
+		left: 27%;
+	}
+	li:nth-of-type(2){
+		top: 42%;
+    	left: 82%;
+	}
+	li:nth-of-type(3){
+		top: 30%;
+		left: 47%;
+	}
+	li:nth-of-type(4){
+		top: 39%;
+    	left: 16%;
+	}
+	li:nth-of-type(5){
+		top: 72%;
+    	left: 68%;
+	}
+    img {
+		width: 81%;
+    	height: 7rem;
+    	margin-top: 2rem;		
+	}
+    h2 {
+		font-size: 1rem;
+	}
+	a {
+		text-decoration: none;
+		color: black;
+		text-align: center;
+	}
+</style>
+<li><a href={link}>
+	<img src={imageSource} alt={objectTitle}>
+	<h2>{objectTitle}</h2>
+</a></li>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,73 +1,31 @@
 <script>
 	export let data;
 	console.log("home");
+	import Object from '../components/Object.svelte'
 
 </script>
 <style>
-	img {
-		width: 81%;
-    	height: 7rem;
-    	margin-top: 2rem;		
-	}
 	div {
 		background: url("/images/indonesia.svg") no-repeat;
 		background-size: 100%;
 		height: 55em;
 	}
-	li {
-		position: absolute;
-		width: 14rem;
-		display: flex;
-		flex-direction: column;
-		height: 13em;
-		background-color: #efefef;
-		border-radius: 0px 5% 5% 5%;
-		transition: .5s ease;
-	}
-	li:hover {
-		transform: scale(1.05);
-
-	}
-	li:nth-of-type(1){
-		top: 68%;
-		left: 27%;
-	}
-	li:nth-of-type(2){
-		top: 42%;
-    	left: 82%;
-	}
-	li:nth-of-type(3){
-		top: 30%;
-		left: 47%;
-	}
-	li:nth-of-type(4){
-		top: 39%;
-    	left: 16%;
-	}
-	li:nth-of-type(5){
-		top: 72%;
-    	left: 68%;
-	}
 	ul {
 		display: flex;
 	}
-	h2 {
-		font-size: 1rem;
-	}
-	a {
-		text-decoration: none;
-		color: black;
-		text-align: center;
-	}
+	
 </style>
 <div>
 <h1>Architectuur van Indonesië</h1>
 <ul>
 	{#each data as source}
-	<li><a href="details/{source.cho.value.slice(-6)}">
+	<Object link="details/{source.cho.value.slice(-6)}" 
+			imageSource={source.imageModel.value} 
+			objectTitle={source.modelName.value}/>
+	<!-- <li><a href="details/{source.cho.value.slice(-6)}">
 		<img src={source.imageModel.value} alt={source.modelName.value}>
 		<h2>{source.modelName.value}</h2>
-	</a></li>
+	</a></li> -->
 	{/each}
 </ul>
 </div>
